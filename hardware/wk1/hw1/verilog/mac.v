5c1a5e66b01d8c9bde962d643d167cdabb4a10f2
ShubhamBirange <shubham.birange@gmail.com>
2024-10-07 19:58:46 -0700

    Updated mac

--- hardware/wk1/hw1/verilog/mac.v
+++ hardware/wk1/hw1/verilog/mac.v
@@ -19,30 +19,39 @@ reg signed [psum_bw-1:0] psum_q;
 reg signed [bw-1:0] a_q;
 reg signed [bw-1:0] b_q;
 
-assign out = format ? 
-                {psum_q[psum_bw-1], psum_q[psum_bw-1] ? 
-                    -psum_q[psum_bw-2:0] : 
-                    psum_q[psum_bw-2:0]} : 
-                psum_q;
+assign out = psum_q;
+
+// Your code goes here
+wire [psum_bw-2:0] multi_mag, psum_mag_q;
+wire psum_sign_q, multi_sign, compare;
+
+assign psum_sign_q = psum_q[psum_bw-1];
+assign psum_mag_q = psum_q[psum_bw-2:0];
+assign compare = psum_mag_q >= multi_mag;
+assign multi_mag = a_q[bw-2:0] * b_q[bw-2:0];
+assign multi_sign = a_q[bw-1] ^ b_q[bw-1];
 
 always @(posedge clk) begin
-    if (reset) begin
-        a_q <= 0;
-        b_q <= 0;
-        psum_q <= 0;
-    end else begin
-        a_q <= A;
-        b_q <= B;
-        
-        if (!format)
-            psum_q <= psum_q + a_q * b_q;
-        else begin
-            if ((a_q[bw-1] == b_q[bw-1]))
-                psum_q <= psum_q + {2'b0, a_q[bw-2:0] * b_q[bw-2:0]};
-            else
-                psum_q <= psum_q - {2'b0, a_q[bw-2:0] * b_q[bw-2:0]};
-        end
+  if (reset) begin
+    psum_q <= 0; 
+    a_q <= 0;
+    b_q <= 0;
+  end else begin
+    a_q <= A;
+    b_q <= B;
+    if (acc) begin
+      if (~format)
+        psum_q <= psum_q + a_q * b_q;
+      else begin
+        case ({compare, multi_sign ^ psum_sign_q})
+                2'b00, 2'b10: psum_q <= {multi_sign, psum_mag_q + multi_mag};
+                2'b01: psum_q <= {multi_sign , multi_mag - psum_mag_q};
+                2'b11: psum_q <= {psum_sign_q, psum_mag_q - multi_mag};
+        endcase
+      end
     end
+  end
 end
+
 
 endmodule
